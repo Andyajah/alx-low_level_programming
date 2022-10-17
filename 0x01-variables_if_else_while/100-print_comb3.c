2eae290b4c22716df5742a6222957cdc23261141
Andyajah <ajahnnamdi93@gmail.com>
2022-10-17 10:42:55 -0700

    My commit 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a program that prints all possible
+ * different combinations of two digits in c programming
+ * Return: Always (0) (Success)
+*/
+
+int main(void)
+{
+	int a, c = '0', b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = c; c <= '9'; c++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+			}
+			if (a == b)
+			{
+				continue;
+			}
+			if (a == '8' && b == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar (',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
