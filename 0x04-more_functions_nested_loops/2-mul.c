023719ff402f1610293222e2c4b487ac567ce005
Andyajah <ajahnnamdi93@gmail.com>
2022-10-20 11:10:54 -0700

    commit fot mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+
+/**
+ * mul -> multiplying two numbers
+ * @a: integer param1
+ * @b: integer param2
+ * Return: success and intiger
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
