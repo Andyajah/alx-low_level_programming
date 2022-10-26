4950b7db12df736f48d50e96e773d02c9b31cb1e
Andyajah <ajahnnamdi93@gmail.com>
2022-10-26 05:40:39 -0700

    commit 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalization function
+ * @x: string param
+ * Return: capitalised version of the string
+*/
+char *cap_string(char *x)
+{	x = "andy, is; in learning c programming"
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"',
+'(', ')', '{', '}' };
+	int len = 13;
+	int a = 0, i;
+
+	while (x[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || s[a - 1] == spc[i] &&
+(s[a] >= 97 && s[a] <= 122))
+					s[a] = s[a] - 32;
+					i++;
+		}
+		a++;
+	}
+	return (x);
+}
